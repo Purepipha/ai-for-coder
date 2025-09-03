426e5515aa7e6f9fbf3773c212141459a76b7d78
Purepipha <13299161883@163.com>
2025-09-03 23:03:49 +0800

    跨域代码增加

---
+++ src/main/java/com/muzi/aiforcoder/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.muzi.aiforcoder.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * CORS跨域配置
+ *
+ * @author muzi
+ * @version 1.0
+ * @date 2025/9/2
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的请求方法
+                .allowedHeaders("*") // 允许的请求头
+                .exposedHeaders("*")
+                .allowCredentials(true) // 允许发送Cookie
+                .maxAge(3600); // 预检请求的缓存时间
+    }
+}
--- src/main/java/com/muzi/aiforcoder/controller/HealthController.java
+++ src/main/java/com/muzi/aiforcoder/controller/HealthController.java
@@ -1,6 +1,7 @@
 package com.muzi.aiforcoder.controller;
 
 import com.muzi.aiforcoder.common.Result;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
