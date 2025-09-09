3ac7176937b66b8ad4ae3039f33ed2604939fbc4
Purepipha <13299161883@163.com>
2025-09-09 22:25:13 +0800

    优化解决前端js精度问题

--- src/main/java/com/muzi/aiforcoder/config/JacksonConfig.java
+++ src/main/java/com/muzi/aiforcoder/config/JacksonConfig.java
@@ -3,32 +3,26 @@ package com.muzi.aiforcoder.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.boot.jackson.JsonComponent;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
-import java.time.format.DateTimeFormatter;
-
-@Configuration
+/**
+ * Spring MVC Json 配置
+ */
+@JsonComponent
 public class JacksonConfig {
 
     /**
-     * 对象映射器
-     *
-     * @return {@link ObjectMapper }
+     * 添加 Long 转 json 精度丢失的配置
      */
     @Bean
-    public ObjectMapper objectMapper() {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        // 所有 Long、long 序列化为字符串
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
         SimpleModule module = new SimpleModule();
         module.addSerializer(Long.class, ToStringSerializer.instance);
-        module.addSerializer(long.class, ToStringSerializer.instance);
-        mapper.registerModule(module);
-        return mapper;
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(module);
+        return objectMapper;
     }
 }
